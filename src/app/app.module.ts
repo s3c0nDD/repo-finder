0f256fdf50cc0326af05ee5b39ad90955ef7d06c
s3c0nDD <s3c0nDD@users.noreply.github.com>
2017-01-12 17:25:55 +0100

    add home and detail components and homepage routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DetailPageComponent } from './detail-page/detail-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    DetailPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+        {
+          path: '',
+          redirectTo: '/home',
+          pathMatch: 'full'
+        },
+        {
+          path: 'home',
+          component: HomePageComponent
+        },
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
