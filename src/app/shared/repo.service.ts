648b0fd3514d18f9002a950ef9d26c5cee1c65f0
s3c0nDD <s3c0nDD@users.noreply.github.com>
2017-01-12 23:26:07 +0100

    repoService: getRepository add and error handling

--- src/app/shared/repo.service.ts
+++ src/app/shared/repo.service.ts
@@ -4,7 +4,7 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from "rxjs";
 import 'rxjs/add/operator/map';
 
-import { Result } from './'
+import { Result, Repo } from './'
 
 @Injectable()
 export class RepoService {
@@ -21,9 +21,33 @@ export class RepoService {
     return this.http
       .get(this.apiPath + 'search/repositories?q=' + query, options)
       .map((res: Response) => {
-        // console.log("Response", res.json());
         return res.json();
-      });
+      })
+      .catch(this.handleError);
+  }
+
+  getRepository(repo: Repo): Observable<Repo> {
+    let headers = new Headers();
+    let options = new RequestOptions({ headers: headers });
+    return this.http
+      .get(this.apiPath + 'repos/' + repo.owner.login + '/' + repo.name, options)
+      .map((res: Response) => {
+        return res.json();
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Observable<any> {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json().body || '';
+      const err = body.message || JSON.stringify(body);
+      errMsg = `${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error('An error occurred:', errMsg);
+    return Observable.throw(errMsg);
   }
 
 }
